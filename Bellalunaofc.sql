468aa002537f056b4f7ed4b474b8c9b0c10e895b
dioclecioricardo-creator <dioclecio.ricardo@escolar.ifrn.edu.br>
2026-06-26 09:47:44 -0300

    Criação da estrutura inicial do banco Bellalunaofc
    Criação das tabelas Funcionarios, Clientes, Produtos, Vendas e Itens_de_vendas

---
+++ Bellalunaofc.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE Bellaluna;
+
+USE Bellaluna;
+
+CREATE TABLE Funcionarios (
+    ID_Funcionario INT PRIMARY KEY,
+    Nome VARCHAR(100) NOT NULL,
+    Cargo VARCHAR(50)
+);
+
+
+CREATE TABLE Clientes (
+    ID_CLIENTE INT PRIMARY KEY,
+    Nome VARCHAR(100) NOT NULL,
+    Telefone VARCHAR(20)
+);
+
+CREATE TABLE Produtos (
+    Codigo_produto INT PRIMARY KEY,
+    Descricao VARCHAR(100) NOT NULL,
+    Preco DECIMAL(10, 2) NOT NULL
+);
+
+
+CREATE TABLE Vendas (
+    identificador VARCHAR(100) PRIMARY KEY,
+    Data DATE NOT NULL,
+    Hora TIME NOT NULL,
+    ID_CLIENTE INT,
+    Codigo INT,
+    FOREIGN KEY (ID_CLIENTE) REFERENCES Clientes(ID_CLIENTE),
+    FOREIGN KEY (Codigo) REFERENCES Produtos(Codigo_produto)
+);
+
+
+
+CREATE TABLE Itens_de_vendas (
+    Codigo INT PRIMARY KEY, 
+    Identificador VARCHAR(20), 
+    Codigo_Produto INT, 
+    FOREIGN KEY (Identificador) REFERENCES Vendas(identificador),
+    FOREIGN KEY (Codigo_Produto) REFERENCES Produtos(Codigo_produto)
+);
